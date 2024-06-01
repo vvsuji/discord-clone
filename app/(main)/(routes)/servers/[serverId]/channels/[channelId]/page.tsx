df1405d9b67c425e6e8477cd8d335217ed06eebd
vvsuji <suji.falcon@gmail.com>
2024-05-31 21:12:18 -0400

    messages working

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from '@/components/chat/chat-header';
 import { ChatInput } from '@/components/chat/chat-input';
+import { ChatMessages } from '@/components/chat/chat-messages';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { RedirectToSignIn } from '@clerk/nextjs';
@@ -43,7 +44,17 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 				serverId={channel.serverId}
 				type='channel'
 			/>
-			<div className='flex-1'>Future Messages</div>
+			<ChatMessages
+				name={channel.name}
+				member={member}
+				chatId={channel.id}
+				type='channel'
+				apiUrl='/api/messages'
+				socketUrl='/api/socket/messages'
+				socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+				paramKey='channelId'
+				paramValue={channel.id}
+			/>
 			<ChatInput
 				name={channel.name}
 				type='channel'
---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { Member, Message, Profile } from '@prisma/client';
+import { ChatWelcome } from './chat-welcome';
+import { useChatQuery } from '@/hooks/use-chat-query';
+import { Loader2, ServerCrash } from 'lucide-react';
+import { Fragment } from 'react';
+
+type MessageWithMemberWithProfile = Message & {
+	member: Member & {
+		profile: Profile;
+	};
+};
+
+interface ChatMessagesProps {
+	name: string;
+	member: Member;
+	chatId: string;
+	apiUrl: string;
+	socketUrl: string;
+	socketQuery: Record<string, string>;
+	paramKey: 'channelId' | 'conversationId';
+	paramValue: string;
+	type: 'channel' | 'conversation';
+}
+
+export const ChatMessages = ({
+	name,
+	member,
+	chatId,
+	apiUrl,
+	socketUrl,
+	socketQuery,
+	paramKey,
+	paramValue,
+	type,
+}: ChatMessagesProps) => {
+	const queryKey = `chat:${chatId}`;
+
+	const { data, fetchNextPage, isFetchingNextPage, hasNextPage, status } =
+		useChatQuery({
+			queryKey,
+			apiUrl,
+			paramKey,
+			paramValue,
+		});
+
+	if (status === 'pending') {
+		return (
+			<div className='flex-1 flex flex-col justify-center items-center'>
+				<Loader2 className='w-7 h-7 text-zinc-500 my-4 animate-spin' />
+				<p className='text-zinc-500 text-xs dark:text-zinc-400'>
+					Loading messages...
+				</p>
+			</div>
+		);
+	}
+
+	if (status === 'error') {
+		return (
+			<div className='flex-1 flex flex-col justify-center items-center'>
+				<ServerCrash className='w-7 h-7 text-zinc-500 my-4' />
+				<p className='text-zinc-500 text-xs dark:text-zinc-400'>
+					Something went wrong!
+				</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex-1 flex flex-col py-4 overflow-y-auto'>
+			<div className='flex-1' />
+			<ChatWelcome type={type} name={name} />
+			<div className='flex flex-col-reverse mt-auto'>
+				{data?.pages?.map((group, i) => (
+					<Fragment key={i}>
+						{group.items.map((message: MessageWithMemberWithProfile) => (
+							<div key={message.id}>{message.content}</div>
+						))}
+					</Fragment>
+				))}
+			</div>
+		</div>
+	);
+};
